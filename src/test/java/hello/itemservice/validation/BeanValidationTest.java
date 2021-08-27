34e8290c1387e538ad2945594b0a9d201557f755
n00nietzsche <n00nietzsche@gmail.com>
2021-08-27 21:12:32 +0900

    스프링 MVC 2편 - 검증-2 #2 Bean Validation - 시작

---
+++ src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,31 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class BeanValidationTest {
+
+    @Test
+    public void beanValidation() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item();
+        item.setItemName(" ");
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            // 여기서 사용하는 메시지는 하이버네이트 validator에서 기본적으로 제공하는 메시지다
+            System.out.println("violation.getMessage() = " + violation.getMessage());
+        }
+    }
+}
